542ffd5be51935852430f5d35affba09a91af81d
ChristianAgren <55452324+ChristianAgren@users.noreply.github.com>
2020-03-09 17:23:29 +0100

    removed unused constructor

--- src/component/dex/main/searchResults.tsx
+++ src/component/dex/main/searchResults.tsx
@@ -8,9 +8,6 @@ interface Props {
 interface State { }
 
 export default class SearchResults extends React.Component<Props, State> {
-    constructor(props: Props){
-        super(props)
-    }
 
     render() {
         return (
