0247296c1281f46cc24bfd91cdabd62415bc13bb
ChristianAgren <55452324+ChristianAgren@users.noreply.github.com>
2020-03-10 22:18:18 +0100

    added loader class

---
+++ src/component/dex/PokeLoad.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react'
+import Pokeload from '../../assets/pokeball-sprite.png'
+
+export default class PokeLoad extends React.Component {
+    render() {
+        return (
+            <div style={loadWrapper}>
+                <img src={Pokeload} alt="pokeball-sprite"/>
+                <p>...loading</p>
+            </div>
+        )
+    }
+}
+
+const loadWrapper: React.CSSProperties = {
+    zIndex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#00000080',
+
+    color: '#E7E7E780'
+}
