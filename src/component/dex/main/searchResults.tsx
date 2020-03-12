37be96bfd954b9fb5f6d064a5d7f216087d748f8
Filip Sunnemar <filipsunnemar@hotmail.com>
2020-03-12 12:14:54 +0100

    Searchbar css for mobile implemented

--- src/component/dex/main/searchResults.tsx
+++ src/component/dex/main/searchResults.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react'
+import "../../searchResultsStyle.css"
 
 interface Props {
     title: string
     value?: number
+    isDesktop: boolean;
 }
 
 interface State { }
@@ -16,7 +18,7 @@ export default class SearchResults extends React.Component<Props, State> {
                     <p>{this.props.title}</p>
                     {(this.props.value && this.props.value >= 5) ? <p>{this.props.value} entries</p> : null}
                 </div>
-                <ul style={searchResultUL}>
+                <ul className={`searchResultUL ${this.props.isDesktop ? "" : "mobile"}`}>
                     {this.props.children}
                 </ul>
             </div>
