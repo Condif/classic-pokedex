e393fe9c4568c16477c26f79f5c1f847fb9f2830
Filip Sunnemar <filipsunnemar@hotmail.com>
2020-03-12 16:06:21 +0100

    searcBarinput responsive

--- src/component/dex/main/searchResults.tsx
+++ src/component/dex/main/searchResults.tsx
@@ -14,11 +14,11 @@ export default class SearchResults extends React.Component<Props, State> {
     render() {
         return (
             <div>
-                <div className={`searchResultTitle ${this.props.isDesktop ? "" : "mobile"}`}>
+                <div className="searchResultTitle">
                     <p>{this.props.title}</p>
                     {(this.props.value && this.props.value >= 5) ? <p>{this.props.value} entries</p> : null}
                 </div>
-                <ul className={`searchResultUL ${this.props.isDesktop ? "" : "mobile"}`}>
+                <ul className="searchResultUL">
                     {this.props.children}
                 </ul>
             </div>
